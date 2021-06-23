510db513c431b0954711a6032be84290565afe4d
Maxwell Weil <maxwellsweil87@gmail.com>
2021-06-23 12:35:16 -0700

    Modified HPC jobs to run using Slurm scheduler

--- hpc-jobs/start_job.py
+++ hpc-jobs/start_job.py
@@ -9,15 +9,13 @@ import argparse
 import json
 import sys
 from itertools import chain
+import subprocess
 
-# import pbstools (Python 3 version)
-sys.path.append("/home/briar.doty/pbstools")
-from pbstools import PythonJob
+from simple_slurm import Slurm
 
-# paths
-python_executable = "/allen/programs/braintv/workgroups/nc-ophys/briar.doty/anaconda3/envs/dlct2/bin/python"
-conda_env = "/allen/programs/braintv/workgroups/nc-ophys/briar.doty/anaconda3/envs/dlct2"
-job_dir = "/allen/programs/braintv/workgroups/nc-ophys/briar.doty/log_files/"
+# Setting paths for python environment to use and where to save output/error logs
+python_executable = "/allen/programs/braintv/workgroups/nc-ophys/max.weil/miniconda3/envs/MaxEnv/bin/python"
+job_dir = "/allen/programs/braintv/workgroups/nc-ophys/max.weil/slurm_logs/"
 
 # args
 parser = argparse.ArgumentParser()
@@ -29,16 +27,15 @@ parser.add_argument("--cases", default=[], nargs="+", type=str)
 
 
 def main(job_title, cases, case, net_name, schemes):
-    
     # script, run_params and job_settings
     with open("job_params.json", "r") as json_file:
         job_params = json.load(json_file)
-    
+
     job_params = job_params[job_title]
     script = job_params["script"]
-    run_params = job_params["run_params"]   
+    run_params = job_params["run_params"]
     job_settings = job_params["job_settings"]
-    
+
     if len(cases) > 0:
         run_params["cases"] = param_arr_helper(cases)
 
@@ -50,30 +47,30 @@ def main(job_title, cases, case, net_name, schemes):
 
     if net_name is not None:
         run_params["net_name"] = net_name
-    
+
     # prepare args
     params_list = list(chain.from_iterable((f"--{k}", str(run_params[k])) for k in run_params))
     params_string = " ".join(params_list)
-    
-    # kick off HPC job
-    PythonJob(
-        script,
-        python_executable,
-        conda_env = conda_env,
-        python_args = params_string,
-        jobname = job_title,
-        jobdir = job_dir,
-        **job_settings
-    ).run(dryrun=False)
+
+    # Kicking off HPC job
+    slurm = Slurm(
+        job_name=f" {net_filepath}",  # Setting name for job
+        output=job_dir + "output.out",  # Setting destination for output log
+        error=job_dir + "error.err",  # Setting destination for error log
+        **job_settings  # Importing all other jobs settings from job_params.json
+    )
+    # Executing slurm job using python path specified above and script/params from job_params.json
+    slurm.sbatch(python_executable + ' ' + script + ' ' + params_string + ' ' + Slurm.SLURM_ARRAY_TASK_ID)
+
 
 def param_arr_helper(param_arr):
-    
     if param_arr is None or len(param_arr) == 0:
         return None
-    
+
     return " ".join(str(p) for p in param_arr)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     args = parser.parse_args()
     print(args)
     main(**vars(args))
