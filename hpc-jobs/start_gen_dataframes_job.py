37e706c58c54c5a6fdfce1a72f58dce3964fdf4e
Maxwell Weil <maxwellsweil87@gmail.com>
2021-08-02 08:36:06 -0700

    Gen_dataframes bug fix

--- hpc-jobs/start_gen_dataframes_job.py
+++ hpc-jobs/start_gen_dataframes_job.py
@@ -18,19 +18,19 @@ parser.add_argument("--net_name", default='sticknet8', type=str, help="Name of n
 parser.add_argument("--dataset", default='cifar10', type=str, help="Name of dataset (ex. cifar10, cifar100)")
 parser.add_argument("--epoch_num", default='150', type=str, help="The epoch number to pull data from")
 
-def main(job_title, net_name, dataset, epoch_num):
-        
+
+def main(net_name, dataset, epoch_num):
     # Open job_params.json file
     with open("job_params.json", "r") as json_file:
         job_params = json.load(json_file)
-    
+
     # Pull out various parameters for chosen job
     job_title = 'gen_dataframes'
     job_params = job_params[job_title]
     script = job_params["script"]
     run_params = job_params["run_params"]
     job_settings = job_params["job_settings"]
-    
+
     # Updating parameters for running chosen job
     run_params["net_name"] = net_name
     run_params["dataset"] = dataset
@@ -49,6 +49,7 @@ def main(job_title, net_name, dataset, epoch_num):
     )
     slurm.sbatch(python_executable + ' ' + script + ' ' + params_string + ' ' + Slurm.SLURM_ARRAY_TASK_ID)
     # Executing slurm job using python path specified above and script/params from job_params.json
+
 
 if __name__ == "__main__":
     # Parsing arguments provided and printing them back
